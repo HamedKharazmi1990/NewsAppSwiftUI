6b9d3342fd718fd4f1234514ac1a66094c98083f
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-02-12 13:38:31 +0100

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Hamed Kharazmi on 12.02.26.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
